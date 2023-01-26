ed0d7c4b15a749b639a49030873b0a018a893eb8
Yaroslav Pazynenko <yaroslavpazynenko@gmail.com>
2023-01-27 00:40:30 +0200

    added theme settings

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
- 
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 
 function App() {
-  return (
-    <div className="app">
-       
-    </div>
-  );
+    const [theme, colorMode] = useMode();
+
+    return (
+        <ColorModeContext.Provider value={colorMode}>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                <div className="app" > </div>
+            </ThemeProvider>
+        </ColorModeContext.Provider>
+    );
 }
 
 export default App;
